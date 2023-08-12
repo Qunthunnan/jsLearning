661b7c337205d282f226545604438273330f3f04
Qunthunnan <bredtv6@gmail.com>
2023-08-12 22:44:12 +0300

    12.08.2023 promt, confirm, alerts, operators, interpolation, git, if else switch

--- js/script.js
+++ js/script.js
@@ -35,10 +35,55 @@
 
 //12.08.2023
 
-const catalog = 'head';
-let num  = 1;
+// const catalog = 'head';
+// let num  = 1;
 
-console.log(`https://www.udemy.com/course/${catalog}/ua`);
+// console.log(`https://www.udemy.com/course/${catalog}/ua`);
 
-console.log(num++);
-console.log(num);
+// console.log(num++);
+// console.log(num);
+
+let numberOfFilms = prompt('Скільки фільмів ви вже подивились?', '');
+
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+personalMovieDB.movies[prompt('Один з останніх переглядених фільмів:', '')] = prompt('Яку оцінку йому поставите?', '');
+personalMovieDB.movies[prompt('Один з останніх переглядених фільмів:', '')] = prompt('Яку оцінку йому поставите?', '');
+
+console.log(personalMovieDB);
+
+const var1 = 5,
+      var2 = 10;
+
+if (var1 > var2) {
+console.log('var1 > var2');
+}
+else {
+console.log('var2 > var1');
+}
+
+(var1 > var2) ? console.log('var1 > var2') : console.log('var2 > var1');
+
+switch (var1) {
+    case 1:
+        console.log('один');
+        break;
+    case 2:
+        console.log('два');
+        break;
+    case 3:
+        console.log('три');
+        break;
+    case var2:
+        console.log('var2');
+        break;
+    default:
+        console.log('нічого');
+        break;
+}
