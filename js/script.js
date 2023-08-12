a47e1862ac728edd2d616e3efdf89aca6c4b1f95
Qunthunnan <bredtv6@gmail.com>
2023-08-12 17:58:03 +0300

    12.08.2023

--- js/script.js
+++ js/script.js
@@ -2,33 +2,43 @@
 
 "use strict";
 
-let bum = 4;
-const ken = 10;
+// let bum = 4;
+// const ken = 10;
 
 
-bum = ken;
-console.log(bum);
+// bum = ken;
+// console.log(bum);
 
-let mass = ['persh', 'другий', 3, '4'];
+// let mass = ['persh', 'другий', 3, '4'];
 
-    for (let i = 0; i < 5; i++) {
-        console.log(mass[i]);
-    }
+//     for (let i = 0; i < 5; i++) {
+//         console.log(mass[i]);
+//     }
 
-const obj = {
-    'Kyrylo': 1000,
-    'Liudmyla': 1111
-};
+// const obj = {
+//     'Kyrylo': 1000,
+//     'Liudmyla': 1111
+// };
 
-const obj2 = {
-    Kyrylo: 2000,
-    Liudmyla: 2111
-};
+// const obj2 = {
+//     Kyrylo: 2000,
+//     Liudmyla: 2111
+// };
 
-console.log(obj2.Kyrylo);
+// console.log(obj2.Kyrylo);
 
-obj2.qunthunnan = 'superUser';
+// obj2.qunthunnan = 'superUser';
 
-console.log(obj2.qunthunnan);
+// console.log(obj2.qunthunnan);
 
-console.log(obj2['qunthunnan']);
+// console.log(obj2['qunthunnan']);
+
+//12.08.2023
+
+const catalog = 'head';
+let num  = 1;
+
+console.log(`https://www.udemy.com/course/${catalog}/ua`);
+
+console.log(num++);
+console.log(num);
